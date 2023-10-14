8d3e4f39c92bc0d2c3e4f641462e5c8acb91794d
sinoxoloxakile <athandiwexakile@gmail.com>
2023-10-14 22:47:30 +0200

    Who was playing in this movie?

---
+++ javascript-network/5-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode === 200) {
+        const characters = JSON.parse(body).characters;
+        printName(characters);
+    } else {
+        console.log('error');
+    }
+});
+
+
+function printName (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    request(arr[i], function (error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+      }
+    });
+  }
+}
